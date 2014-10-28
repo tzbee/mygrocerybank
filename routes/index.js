5d0fa08a567e6a818c6e8cd08cb54385665ce53f
Touzbi <tolbimine@gmail.com>
2014-10-28 19:06:59 +0200

    clean up and added minimal ui

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
 
-/* GET Hello page. */
-router.get('/hello',function(req,res){
-	res.render('hello',{title:'Da hello page!'});
+/* GET Index page. */
+router.get('/',function(req,res){
+	res.render('index',{title:'Da hello page!'});
 });
 
 /* GET all users. */
-router.get('/users',function(req,res){
+router.post('/items',function(req,res){
 	var db = req.db;
 	var collection = db.get('usercollection');
 	collection.find({},{},function(e,docs){
