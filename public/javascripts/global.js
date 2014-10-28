106e45377615bf56612d522808c96f43e2ff7a4f
Touzbi <tolbimine@gmail.com>
2014-10-29 01:47:48 +0200

    Adding item client side almost working

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,14 +1,30 @@
 var mgb = {};
 
-(function(){
+(function(window,document,$){
 
-	var addItemBtn = $('#addItemBtn'),
-		itemName = $('#itemName'),
+	var $addItemBtn = $('#addItemBtn'),
+		$itemName = $('#itemName'),
+		$messageLabel = $('#messageLabel'),
 		itemResourceURL = '/items';
 
-	mgb.addItem = function(item){
-		// TODO
-		console.log('Adding item ' + item.name);
+	mgb.addItem = function(){
+		// XXX To fix
+
+		var itemName = $itemName.value()
+
+		$.post(itemResourceURL,{name: itemName}).done(function(){
+			messageLabel.html('Item ' + itemName +' created');
+		});
+
 	};
 
-})
+	$(document).ready(function(){
+		// XXX To fix
+		$addItemBtn.click(function(e){
+			console.log('HEOOOO');
+		});
+
+	});
+
+
+})(window,document,jQuery);
