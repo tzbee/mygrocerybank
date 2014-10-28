5cf8328b914e9d282a739e263b00a51c9b75a43f
Touzbi <tolbimine@gmail.com>
2014-10-29 01:18:05 +0200

    Added global javascript file and linked to all views

---
+++ public/javascripts/global.js
@@ -0,0 +1,14 @@
+var mgb = {};
+
+(function(){
+
+	var addItemBtn = $('#addItemBtn'),
+		itemName = $('#itemName'),
+		itemResourceURL = '/items';
+
+	mgb.addItem = function(item){
+		// TODO
+		console.log('Adding item ' + item.name);
+	};
+
+})
